9d73d9aed371052767f79debd35359e957abe138
Mohammad Ali <67081000+jixaw31@users.noreply.github.com>
2025-06-13 03:08:48 +0330

    Update page.tsx

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -49,7 +49,8 @@ export default function ChatPage() {
             setLoadingMessages,
             conversations,
             loadingMessages, spinnerLoading,
-            fetchConversations, isMinimized} = useChat();
+            fetchConversations, isMinimized,
+            showAuthMessage, authMessage, fadeOut} = useChat();
   
 
   // Start a new conversation
@@ -201,7 +202,17 @@ export default function ChatPage() {
           inputRef={inputRef}
           setSpinnerLoading={setSpinnerLoading}
         />
-            
+        {/* Top-right message */}
+          {showAuthMessage && (
+            <div
+              className={`absolute top-4 mr-10 right-4 p-3 rounded ${!authMessage.includes("out")?"bg-green-600":"bg-gray-400"} text-white text-sm shadow-lg transition-opacity duration-1000 ${
+                fadeOut ? 'opacity-0' : 'opacity-100'
+              }`}
+            >
+              {authMessage}
+            </div>
+          )}
+        {/* Centered content */} 
         <GlassyFooter />
       </div>
       
