940afec65b285c0fa458142277c3b5b22b72f674
Mohammad Ali <67081000+jixaw31@users.noreply.github.com>
2025-06-12 21:37:25 +0330

    Update page.tsx

--- app/_not-found/page.tsx
+++ app/_not-found/page.tsx
@@ -1,9 +1,11 @@
-// app/_not-found/page.tsx
+import { Suspense } from 'react';
 export default function NotFound() {
   return (
+    <Suspense fallback={<div>Loading…</div>}>
     <main className="p-8">
       <h1 className="text-4xl font-bold">Not Found</h1>
       <p>Sorry, the requested resource could not be found.</p>
     </main>
+    </Suspense>
   );
 }
