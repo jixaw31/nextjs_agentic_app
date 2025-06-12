fc0dadcde83fec9fde5c3262f90d78ebae742ba3
Mohammad Ali <67081000+jixaw31@users.noreply.github.com>
2025-06-12 21:36:51 +0330

    Update page.tsx

--- app/404/page.tsx
+++ app/404/page.tsx
@@ -1,9 +1,11 @@
-// app/404/page.tsx
+import { Suspense } from 'react';
 export default function Custom404() {
   return (
+    <Suspense fallback={<div>Loading…</div>}>
     <main className="p-8">
       <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
       <p>The page you are looking for does not exist.</p>
     </main>
+    </Suspense>
   );
 }
