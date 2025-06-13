a5cbdd83350600a4fa67cca00d5c107c89df458f
unknown <jixaw31@yahoo.com>
2025-06-13 18:55:46 +0330

    ss

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function ChatPage() {
     {/* Top-right message */}
           {showAuthMessage && (
             <div
-              className={`fixed top-4 h-4/5 right-4 p-3 rounded ${!authMessage.includes("out")?"bg-green-600":"bg-gray-400"} text-white text-sm shadow-lg transition-opacity duration-1000 ${
+              className={`fixed top-4 mr-10 mt-10 right-4 p-3 rounded ${!authMessage.includes("out")?"bg-green-600":"bg-gray-400"} text-white text-sm shadow-lg transition-opacity duration-1000 ${
                 fadeOut ? 'opacity-0' : 'opacity-100'
               }`}
             >
