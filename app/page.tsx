86781af29c59cfae7707e848bf68cdbb542ff4a0
Mohammad Ali <67081000+jixaw31@users.noreply.github.com>
2025-06-13 17:06:00 +0330

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
-export default function ChatPage() {
+"use client";
+import { useChat } from '../context/AppContext';
 
+export default function ChatPage() {
+  const { showAuthMessage, authMessage, fadeOut } = useChat();
   return (
     
   <div className="relative h-screen flex items-center justify-center">
     
     <div className="h-2/3 text-2xl font-semibold">Landing Page</div>
+    {/* Top-right message */}
+          {showAuthMessage && (
+            <div
+              className={`absolute top-4 mr-10 right-4 p-3 rounded ${!authMessage.includes("out")?"bg-green-600":"bg-gray-400"} text-white text-sm shadow-lg transition-opacity duration-1000 ${
+                fadeOut ? 'opacity-0' : 'opacity-100'
+              }`}
+            >
+              {authMessage}
+            </div>
+          )}
+    {/* Centered content */}
   </div>
 );
 }
